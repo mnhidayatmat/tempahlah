d122831a24cd75a384a2d5c77a2e3bc981f845b4
mnhidayat91 <drhidayatmat@gmail.com>
2026-05-02 08:20:06 +0800

    refactor(ui): unify typography on Geist + Geist Mono
    Drops Instrument Serif (display) and JetBrains Mono (mono) in favour of an
    all-Geist system. Display sizes tightened (48/32/22 from 56/40/28) and weight
    bumped to 600 for sharper hierarchy. Kicker switches to sans, mono picks up
    tabular-nums.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="manifest" href="/manifest.webmanifest">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Geist:wght@400;500;600;700&family=Instrument+Serif&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Geist:wght@400;500;600;700&family=Geist+Mono:wght@400;500&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('head')
 </head>
